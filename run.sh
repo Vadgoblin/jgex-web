f878a3d668e88c05de5ba107eb6c5363ae05e065
vadgoblin@arch <vadgoblin@github.com>
2026-09-02 20:17:57 +0200

    update run script to use nix shell

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-npx http-server jgex-web/ -p 8080 -c-1 --cors
+# npx http-server jgex-web/ -p 8080 -c-1 --cors
+nix shell nixpkgs#caddy -c caddy file-server --listen :8080 --root jgex-web/
