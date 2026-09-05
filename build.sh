55fdc595fe900fe920db8e0652cd811db69d5550
vadgoblin@arch <vadgoblin@github.com>
2026-09-05 13:26:54 +0200

    update build helper script to copy help pages to web root

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 (cd ./Java-Geometry-Expert && nix shell nixpkgs#jdk17 -c ./gradlew clean jar)
 cp ./Java-Geometry-Expert/build/libs/Java-Geometry-Expert-0.87.jar ./jgex-web/jgex.jar
+
+cp -r Java-Geometry-Expert/src/main/resources/docs/help/ ./jgex-web/
