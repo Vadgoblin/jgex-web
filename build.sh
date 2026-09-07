a33cd1788072ef6cd50b6e6a72c0701711a4589b
vadgoblin@arch <vadgoblin@github.com>
2026-09-07 10:05:57 +0200

    update helper scripts repalce nix with docker

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-(cd ./Java-Geometry-Expert && nix shell nixpkgs#jdk17 -c ./gradlew clean jar)
+docker run --rm \
+  -v "$HOME/.gradle":/root/.gradle \
+  -v "$(pwd)/Java-Geometry-Expert":/app \
+  -w /app \
+  eclipse-temurin:17 ./gradlew clean jar
+
 cp ./Java-Geometry-Expert/build/libs/Java-Geometry-Expert-0.87.jar ./jgex-web/jgex.jar
 
 cp -r Java-Geometry-Expert/src/main/resources/docs/help/ ./jgex-web/
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # npx http-server jgex-web/ -p 8080 -c-1 --cors
-nix shell nixpkgs#caddy -c caddy file-server --listen :8080 --root jgex-web/
+# nix shell nixpkgs#caddy -c caddy file-server --listen :8080 --root jgex-web/
+
+docker run --rm -p 8080:8080 -v "$(pwd)/jgex-web":/srv -w /srv caddy:alpine caddy file-server --listen :8080 --access-log
